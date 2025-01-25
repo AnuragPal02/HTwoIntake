ed0a412e808e4e3493f21f45705cbf3fe328f087
AnuragPal <anuragpal7754@gmail.com>
2025-01-25 10:53:45 +0530

    register and login commit

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,16 +1,31 @@
 
+import { UNSAFE_RouteContext } from "react-router-dom";
 import { User } from "../models/users.models.js";
 import apiErrors from "../utils/apiErrors.js";
 
 import apiResponse from "../utils/apiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-//  ------register new user ----
+const generateAccessAndRefreshToken = async(userId)=>{
+       try {
+         const user = await User.findById(userId);
+         if (!user) {
+          throw new apiErrors(`can't find user with the id: ${userId}`);
+         }
+         const accessToken = user.generateAccessToken();
+         const refreshToken = user.generateRefreshToken();
 
-// first take the username, email, password
-// next check whether all the fields are in the correct format
-// next check if the user already exist
-// if user exist return login message else proceed forward
-// next hash the password and store it in the database
+        //  console.log(accessToken,'in function');
+        //  console.log(refreshToken,'in function'); 
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave:false});
+        
+        return {accessToken,refreshToken};
+       }catch (error) {
+          throw new apiErrors(400,'something went wrong while generating tokens');
+       }
+}
 
 
 const registerUser = async(req,res)=>{
@@ -18,6 +33,8 @@ const registerUser = async(req,res)=>{
      try {
       // taking the form details
       const {username, email, password, phoneNumber} = req.body;
+
+      // console.log(req.body);
     
       // checking whether any of the required field is empty 
       if(
@@ -45,12 +62,74 @@ const registerUser = async(req,res)=>{
 
        const user = await User.findById(newUser._id).select('-password -refreshToken');
 
+      console.log("user registered successfully..");
        return res.status(200).json(
          new apiResponse(200,user,"User Registered successfully!!")
        )
      } catch (error) {
-       throw new apiErrors('404', 'could not register...');
+      console.error("Error during user registration:", error); // Log for debugging
+      return res.status(500).json(
+        new apiResponse(500, null, "Internal server error: could not register the user")
+      );
      }
 }
 
-export {registerUser};
+const loginUser = asyncHandler(async(req,res)=>{
+  try {
+    // steps to achieve login
+    // step - 1 : take the input and validate it 
+    // step - 2 : now check whether the user exists in the database using username or email
+    // step - 3 : if exists then login
+    
+    const {username, email, password} = req.body;
+    console.log(req.body);
+
+    if(!username && !email){
+      throw new apiErrors(404,'Please enter either username or email');
+    }
+    if(!password){
+      throw new apiErrors(401,'Password is required..');
+    }
+    
+    const user = await User.findOne({$or:[{username},{email}]});
+    if(!user){
+      throw new apiErrors(401,'user does not exist, please register..');
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+      throw new apiErrors(401,'Password is not correct...');
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+    
+    const loggedInUser = await User.findById(user._id).select('-password -refreshToken');
+
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+     console.log("login successfull...");
+     return res
+     .status(200)
+     .cookie('accessToken',accessToken,options)
+     .cookie('refreshToken',refreshToken,options)
+     .json(
+      new apiResponse(
+        200,
+        {
+          user: loggedInUser,accessToken, refreshToken
+        },
+        'User logged in successfully..'
+      )
+     )
+  } catch (error) {
+    console.error("something went wrong while loggin..",error);
+    res.status(501).json(
+       new apiResponse(501,'couldnot login...')
+    )
+  }
+})
+
+export {registerUser,loginUser};
